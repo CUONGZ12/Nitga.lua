d1e4a89d21cb37de64394b61e24e8d1021a3c31b
CUONGZ12 <dubaicityhihi@gmail.com>
2026-02-22 19:29:37 +0700

    Nitga.lua

--- Nitga.lua
+++ Nitga.lua
@@ -5,24 +5,6 @@ local player = Players.LocalPlayer
 
 math.randomseed(tick())
 
--- ===== Tp player =====
-task.spawn(function()
-    while true do
-        task.wait(0.1)
-
-        local character = player.Character
-        if character then
-            local hrp = character:FindFirstChild("HumanoidRootPart")
-            if hrp then
-                local offsetX = math.random(-300, 300)
-                local offsetZ = math.random(-300, 300)
-
-                local newPosition = hrp.Position + Vector3.new(offsetX, 0, offsetZ)
-                hrp.CFrame = CFrame.new(newPosition)
-            end
-        end
-    end
-end)
 
 -- ===== Auto chat spam =====
 local TextChatService = game:GetService("TextChatService")
